2e5326064ecce6af68c09cd4a01ffdd985bdde21
Irina <i.ivlieva2011@yandex.ru>
2019-11-02 12:18:02 +0400

    Initial commit

---
+++ app/src/main/java/com/example/buhat/ListVulkanovFragment.java
@@ -0,0 +1,25 @@
+package com.example.buhat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListVulkanovFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_list_vulkanov, container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
